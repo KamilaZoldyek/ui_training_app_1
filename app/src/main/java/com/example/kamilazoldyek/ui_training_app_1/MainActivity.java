39629e8000a3500ac119ccc574b51b544f6124fb
KamilaZoldyek <kmilawicked@hotmail.com>
2018-12-20 17:52:46 -0200

    more layout changes

---
+++ app/src/main/java/com/example/kamilazoldyek/ui_training_app_1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kamilazoldyek.ui_training_app_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
